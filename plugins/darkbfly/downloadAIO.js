5d1840ab33611bd59f5d0c69a7f2d6c92e022e78
xuwb <xuwb@superb-pay.com>
2024-09-09 10:46:51 +0800

    初始化

---
+++ plugins/darkbfly/downloadAIO.js
@@ -0,0 +1,80 @@
+/**作者
+ * @author darkbfly
+ * 插件名
+ * @name downloadAIO
+ * 组织名  预留字段，未来发布插件会用到
+ * @team darkbfly
+ * 版本号
+ * @version 1.0.5
+ * 说明
+ * @description demo
+ * 触发正则   在bncr 所有的rule都被视为正则
+ * @rule (https://h5.pipix.com[.a-z/A-Z0-9/]*)
+ * @rule (https://v.douyin.com[.a-zA-Z0-9/]*)
+ * @rule (https://v.kuaishou.com[.a-zA-Z0-9/]*)
+ * // 是否管理员才能触发命令
+ * @admin true
+ * // 是否发布插件，预留字段，可忽略
+ * @public false
+ * // 插件优先级，越大优先级越高  如果两个插件正则一样，则优先级高的先被匹配
+ * @priority 9999
+ * // 是否禁用插件
+ * @disable false
+ * // 是否服务模块，true不会作为插件加载，会在系统启动时执行该插件内容
+ * @service false
+ */
+
+const axios = require('axios');
+async function downloadAIO(s, url) {
+    const options = {
+        method: 'POST',
+        url: 'https://auto-download-all-in-one.p.rapidapi.com/v1/social/autolink',
+        headers: {
+            'x-rapidapi-key': 'd921658e41msh5bd3d82480436e7p164560jsn2e1d11f7c6d9',
+            'x-rapidapi-host': 'auto-download-all-in-one.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+        data: {
+            url: url
+        }
+    };
+
+    try {
+        var videoUrl = null;
+        const response = await axios.request(options);
+        await s.reply(response.data.title);
+        var videoPriority = ["hd_no_watermark", "no_watermark", "watermark"]
+        for (const element1 of videoPriority) {
+            if (videoUrl) break;
+            for (const element of response.data.medias) {
+                if (element.quality == element1) {
+                    videoUrl = element.url;
+                    break;
+                }
+            }
+        }
+        if (videoUrl) {
+            await s.reply({
+                type: "video",
+                path: videoUrl
+            });
+        } else {
+            await s.reply("下载地址未找到");
+        }
+    } catch (error) {
+        await s.reply("解析异常！");
+        console.error(error);
+    }
+}
+
+module.exports = async s => {
+    console.log(s.param(0));
+    await s.reply("解析中...");
+    // downloadAIO(s, s.param(0))
+
+    s.reply({
+        type: 'image', // video
+        msg: '图片来啦',
+        path: 'https://pic3.zhimg.com/v2-58d652598269710fa67ec8d1c88d8f03_r.jpg',
+    });
+};
---
+++ plugins/darkbfly/myenc.js
@@ -0,0 +1,65 @@
+/**作者
+ * @author darkbfly
+ * 插件名
+ * @name myenc
+ * 组织名  预留字段，未来发布插件会用到
+ * @team darkbfly
+ * 版本号
+ * @version 1.0.5
+ * 说明
+ * @description myenc
+ * 触发正则   在bncr 所有的rule都被视为正则
+ * @rule ^(mydec|myenc)\s+([^ \n]+)$
+ * // 是否管理员才能触发命令
+ * @admin true
+ * // 是否发布插件，预留字段，可忽略
+ * @public false
+ * // 插件优先级，越大优先级越高  如果两个插件正则一样，则优先级高的先被匹配
+ * @priority 9999
+ * // 是否禁用插件
+ * @disable false
+ * // 是否服务模块，true不会作为插件加载，会在系统启动时执行该插件内容
+ * @service false
+ */
+
+function encData(strData) {
+    let array = [];
+    for (let i = 0; i < strData.length; i++) {
+        array.push(256 - (strData.charCodeAt(i) % 256));
+    }
+    return btoa(String.fromCharCode(...array));
+}
+function decData(strData) {
+    let array = atob(strData).split('').map(c => 256 - (c.charCodeAt(0) % 256));
+    let gbkDecodedStr;
+
+    // 使用GBK解码
+    try {
+        return new TextDecoder('utf-8').decode(new Uint8Array(array));
+    } catch (err) {
+        console.error("GBK decoding failed.");
+        return "GBK Decoding failed!";
+    }
+}
+
+function isValidJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+module.exports = async s => {
+    console.log(s.param(1));
+    console.log(s.param(2));
+    if (s.param(1) == "myenc") {
+        await s.reply("加密字符串为[" + encData(s.param(2)) + "]");
+    } else if (s.param(1) == "mydec") {
+        var data = decData(s.param(2))
+        if (isValidJSON(data)) {
+            await s.reply("解密字符串为JSON\n" + JSON.stringify(JSON.parse(data), null, 2));
+        } else { await s.reply("解密字符串为[" + data + "]"); }
+    }
+};
